d3aceeaa0e3b028aa0722929cfc6563c3973bda4
R2D2VaderBeef <52345992+R2D2VaderBeef@users.noreply.github.com>
2022-12-17 16:02:23 +0000

    simple server, self update with nodemon .mistbot

---
+++ index.js
@@ -0,0 +1,45 @@
+// imports
+// file system stuff
+require("dotenv").config();
+const fs = require("fs");
+
+// webserver
+const express = require("express");
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+// git
+const simpleGit = require("simple-git");
+const git = simpleGit.default();
+
+
+// server code
+http.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
+
+app.use(function (req, res, next) {
+    console.log("[" + new Date().toLocaleString() + "] [app.use] Route: " + req.url + " | IP: " + req.ip + " | Forwarded-For: " + req.headers["x-forwarded-for"]);
+    next();
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(process.cwd() + "/index.html")
+});
+
+app.get("/patch", async (req, res) => {
+    if (req.query.token == process.env.TOKEN) {
+        res.send("Updating from Git now.");
+        console.log("Running git pull now.");
+        await git.pull(["origin", "master"]);
+        let hash = await git.revparse(["HEAD"]);
+        console.log("Now up-to-date with " + hash.slice(0, 7));
+
+        let string = "Last restart: " + new Date().toLocaleString(); 
+        fs.writeFile("restart.mistbot", string, function() {return;});
+    }
+    else {
+        res.status(401).send("Goodbye.");
+    }
+})
